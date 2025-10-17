459e0d5f79aff23ececd16900222d7080d291cfa
kevintannum <117162028+kevintannum@users.noreply.github.com>
2025-10-17 16:20:21 -0700

    Update Bards.py

--- Bards.py
+++ Bards.py
@@ -1,3 +1,25 @@
+"""
+Every evening villagers in a small village gather around a big fire and sing songs.
+A prominent member of the community is the bard. Every evening, if the bard is present, he sings a brand new song that no villager has heard before, and no other song is sung that night. In the event that the bard is not present, other villagers sing without him and exchange all songs that they know.
+
+Given the list of villagers present for consecutive evenings, output all villagers that know all songs sung during that period.
+
+Input
+The first line of input contains an integer, the number of villagers. The villagers are numbered 1 to N. Villager number 1
+ is the bard.
+
+The second line contains an integer the number of evenings.
+
+The next lines contain the list of villagers present on each of the 
+ evenings. Each line begins with a positive integer the number of villagers present that evening, followed by 
+ N positive integers separated by spaces representing the villagers.
+
+No villager will appear twice in one night and the bard will appear at least once across all nights.
+
+Output all villagers that know all songs, including the bard, one integer per line in ascending order.
+"""
+
+
 V = int(input())
 E = int(input())
 
@@ -30,3 +52,4 @@ for present in evenings:
 for v in range(1, V + 1):
     if len(known_songs[v - 1]) == total_songs:
         print(v)
+
